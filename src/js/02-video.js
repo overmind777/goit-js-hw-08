6845cecbab81837be94bec1c0ee2d732a4db91a9
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-09-23 13:28:35 +0300

    bugfix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,22 +3,17 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const updateCurrentTime = throttle(time => {
+const updateCurrentTime = time => {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
-});
+};
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.on('timeupdate', data => {
-  const currentTime = data.seconds;
-  updateCurrentTime(currentTime);
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    updateCurrentTime(currentTime);
+  }, 1000)
+);
 
 const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 if (savedTime !== null) {
